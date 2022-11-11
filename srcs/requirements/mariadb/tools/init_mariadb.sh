4d3588fac8944e09c075c8d176c3ef9ad3211424
Yub_2 <hyeok230@naver.com>
2022-11-11 23:10:24 +0900

    fix mariadb

--- srcs/requirements/mariadb/tools/init_mariadb.sh
+++ srcs/requirements/mariadb/tools/init_mariadb.sh
@@ -9,11 +9,13 @@ if [ $? -ne 0 ]; then
 	mysql -e "CREATE DATABASE $DB_NAME";
 	mysql -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD'";
 	mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%'";
-	mysql -e "FLUSH PRIVILEGES";	
+	# mysql -e "FLUSH PRIVILEGES";	
 	mysql -e "ALTER USER '$DB_ROOT'@'localhost' IDENTIFIED BY '$DB_ROOTPWD'";
+	mysql -e "FLUSH PRIVILEGES";
 #	mysql $DB_NAME -u$DB_ROOT -p$DB_ROOTPWD;
-	mysqladmin -u$DB_ROOT -p$DB_ROOTPWD shutdown
 	touch /var/lib/mysql/.setup
 fi
 
-exec /usr/bin/mysqld_safe
+mysqladmin -u$DB_ROOT -p$DB_ROOTPWD shutdown
+
+exec mysqld
